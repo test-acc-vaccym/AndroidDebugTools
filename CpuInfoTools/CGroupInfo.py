9f6571e814aa85eff4fa33e65683186ad43c7948
高建武 <gaojianwu@meizu.com>
2016-10-19 15:57:33 +0800

    Add SimpleCpuDebugApp

--- CpuInfoTools/CGroupInfo.py
+++ CpuInfoTools/CGroupInfo.py
@@ -1,1 +1,87 @@
-# Not Ready yet
+import shlex
+import os
+
+list_cpuset_name = ['performance', 'foreground', 'foreground-ui', 'foreground-boost', 'background', 'background-system']
+
+list_cpuset_path = ['/dev/cpuset/performance/tasks',
+                    '/dev/cpuset/foreground/tasks',
+                    '/dev/cpuset/foreground/ui/tasks',
+                    '/dev/cpuset/foreground/boost/tasks',
+                    '/dev/cpuset/background/tasks',
+                    '/dev/cpuset/background-system/tasks',
+                    '/dev/cpuset/tasks']
+
+list_cpuset = []
+list_cpuset_performance_task = []
+list_cpuset_foreground_task = []
+list_cpuset_foreground_ui_task = []
+list_cpuset_foreground_boost_task = []
+list_cpuset_background_task = []
+list_cpuset_background_system_task = []
+list_cpuset_default_task = []
+
+
+def execute_command(cmdstring, list=None, cwd=None, timeout=None, shell=True):
+    if shell:
+        cmdstring_list = cmdstring
+    else:
+        cmdstring_list = shlex.split(cmdstring)
+
+    sub = os.popen(cmdstring_list)
+    return sub.readlines()
+
+
+# 获取此cpuset中的所有 Task
+def get_tasks_form_cpuset():
+    list_cpuset_performance_task = execute_command("adb shell cat " + list_cpuset_path[0])
+    list_cpuset_foreground_task = execute_command("adb shell cat " + list_cpuset_path[1])
+    list_cpuset_foreground_ui_task = execute_command("adb shell cat " + list_cpuset_path[2])
+    list_cpuset_foreground_boost_task = execute_command("adb shell cat " + list_cpuset_path[3])
+    list_cpuset_background_task = execute_command("adb shell cat " + list_cpuset_path[4])
+    list_cpuset_background_system_task = execute_command("adb shell cat " + list_cpuset_path[5])
+    list_cpuset_default_task = execute_command("adb shell cat " + list_cpuset_path[6])
+
+    print("------------------Begin performance_task info ---------------------")
+    get_cpuset_info_from_list(list_cpuset_performance_task)
+    print("------------------End performance_task info------------------------")
+
+    print("--------------Begin foreground_boost_task info ------------------ ")
+    get_cpuset_info_from_list(list_cpuset_foreground_boost_task)
+    print("--------------End foreground_boost_task info----------------------------")
+
+    print("---------------Begin foreground_ui_task info  ------------------- ")
+    get_cpuset_info_from_list(list_cpuset_foreground_ui_task)
+    print("---------------End foreground_ui_task info--------------------------")
+
+    print("-----------------Begin foreground_task info ----------------------")
+    get_cpuset_info_from_list(list_cpuset_foreground_task)
+    print("-------------------End foreground_task info----------------------")
+
+    print("-----------------Begin background_task info  ---------------------")
+    get_cpuset_info_from_list(list_cpuset_background_task)
+    print("-----------------End background_task info-------------------------")
+
+    print("-------------Begin background_system_task info ---------------------")
+    get_cpuset_info_from_list(list_cpuset_background_system_task)
+    print("-------------End background_system_task info-----------------------------")
+
+    print("-------------Begin list_cpuset_default_task info ---------------------")
+    get_cpuset_info_from_list(list_cpuset_default_task)
+    print("-------------End list_cpuset_default_task info-----------------------------")
+
+
+def get_cpuset_info_from_list(task_list):
+    ps_info = execute_command("adb shell ps")
+    for task in task_list:
+        for process in ps_info:
+            if(task.strip() in process):
+                print("Task " + task + " ProcessInfo = " + process)
+
+
+# 获取cpuset对应的cpu信息
+def get_cpus_for_cpuset():
+    return
+
+
+if __name__ == "__main__":
+    get_tasks_form_cpuset()
---
+++ SimpleCpuDebugApp/app/src/main/java/com/gracker/simpleapplication/MainActivity.java
@@ -0,0 +1,57 @@
+package com.gracker.simpleapplication;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = "MainActivity";
+    public static final int THREAD_NUMBER = 10;
+
+    private TextView mTextView = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTextView = (TextView) findViewById(R.id.textView);
+
+        for (int i = 0; i < THREAD_NUMBER; i++) {
+            createNewThread();
+        }
+
+        updateComputeTime();
+    }
+
+    public void updateComputeTime() {
+        mTextView.setText("当前线程数量为 : " + THREAD_NUMBER  );
+    }
+
+    // 使用 while 循环来一直进行斐波那契数列的计算.
+    private void createNewThread() {
+        Thread newThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (true) {
+                    fibonacciRecursive(100);
+                    Log.v(TAG, Thread.currentThread().getName());
+                }
+            }
+        });
+
+        newThread.setPriority(Thread.MAX_PRIORITY);
+        newThread.start();
+    }
+
+    // 使用递归来计算斐波那契数列
+    public int fibonacciRecursive(int n) {
+        if (n <= 2) {
+            return 1;
+        }
+        return fibonacciRecursive(n - 1) + fibonacciRecursive(n - 2);
+    }
+
+}
